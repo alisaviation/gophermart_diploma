be366d2c0511d8b23c061b29fc1e3065ef93ca7b
RahatMelsov <rahatmelsovforjob@gmai.com>
2025-03-16 16:11:40 +0500

    FIX: Добавили логи при проверке номера заказа

--- internal/models/config.go
+++ internal/models/config.go
@@ -3,7 +3,7 @@ package models
 type EnvVariables struct {
 	RunAddress           string `env:"RUN_ADDRESS"`
 	RootUrl              string `env:"ROOT_URL"`
-	AccuralSystemAddress string `env:"ACCURAL_SYSTEM_ADDRESS"`
+	AccuralSystemAddress string `env:"ACCRUAL_SYSTEM_ADDRESS"`
 	DataBaseURL          string `env:"DATABASE_URI"`
 	Secret               string `env:"SECRET_KEY"`
 }
