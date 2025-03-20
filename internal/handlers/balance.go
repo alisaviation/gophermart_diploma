345b7f77c4389fb1eeef8dd0ef96560fc8e2bb72
RahatMelsov <rahatmelsovforjob@gmai.com>
2025-03-20 15:18:20 +0500

    FIX: Добавили логи для проверки ошибки при получении бонуса

--- internal/handlers/balance.go
+++ internal/handlers/balance.go
@@ -9,6 +9,8 @@ func (h *Handler) GetBalance(c *gin.Context) {
 	id := c.GetUint("userId")
 	balance, localErr := h.serv.GetBalance(&id)
 
+	h.conf.GetLogger().Info("balance info", zap.Float64("balance.Withdraw", balance.Withdraw), zap.Float64("balance.Current", balance.Current))
+
 	if localErr != nil {
 		h.conf.GetLogger().Error("error", zap.String("error", localErr.Error))
 		c.JSON(localErr.Code, localErr.Error)
