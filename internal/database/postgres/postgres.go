f4b47cfb56d3aa2f45fd1a813418d52e9c824960
eaobolenskaya <eaobolenskaya@avito.ru>
2025-07-17 15:18:15 +0300

    add migrations

--- internal/database/postgres/postgres.go
+++ internal/database/postgres/postgres.go
@@ -1,10 +1,15 @@
 package postgres
 
 import (
-	"context"
 	"database/sql"
 	"errors"
 	"fmt"
+	"path/filepath"
+	"runtime"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 var (
@@ -17,56 +22,82 @@ type PostgresStorage struct {
 
 func NewPostgresDatabase(db *sql.DB) (*PostgresStorage, error) {
 	storage := &PostgresStorage{db: db}
-	if err := storage.createTable(context.Background(), db); err != nil {
-		return nil, fmt.Errorf("failed to create users table: %w", err)
+	if err := storage.runMigrations(); err != nil {
+		return nil, fmt.Errorf("failed to run migrations: %w", err)
 	}
 	return storage, nil
 }
-
-func (p *PostgresStorage) createTable(ctx context.Context, db *sql.DB) error {
-	tx, err := db.BeginTx(ctx, nil)
-	if err != nil {
-		return err
+func (p *PostgresStorage) runMigrations() error {
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		return fmt.Errorf("failed to get current file path")
 	}
-	defer tx.Rollback()
+	migrationsPath := filepath.Join(filepath.Dir(filename), "migrations")
 
-	_, err = tx.ExecContext(ctx, `
-		CREATE TABLE IF NOT EXISTS users (
-		    id SERIAL PRIMARY KEY,
-			login TEXT NOT NULL UNIQUE,
-			password_hash TEXT NOT NULL
-		)
-	`)
+	driver, err := postgres.WithInstance(p.db, &postgres.Config{})
 	if err != nil {
-		return fmt.Errorf("failed to create users table: %w", err)
+		return fmt.Errorf("failed to create postgres driver: %w", err)
 	}
 
-	_, err = tx.ExecContext(ctx, `
-		CREATE TABLE IF NOT EXISTS orders (
-		    id SERIAL PRIMARY KEY,
-			user_id INTEGER NOT NULL REFERENCES users(id),
-			number TEXT NOT NULL UNIQUE,
-			status TEXT NOT NULL,
-			accrual DECIMAL(10, 2) DEFAULT 0,
-		    uploaded_at TIMESTAMP WITH TIME ZONE NOT NULL
-		)
-	`)
+	m, err := migrate.NewWithDatabaseInstance(
+		fmt.Sprintf("file://%s", migrationsPath),
+		"postgres", driver)
 	if err != nil {
-		return fmt.Errorf("failed to create orders table: %w", err)
+		return fmt.Errorf("failed to create migrate instance: %w", err)
 	}
 
-	_, err = tx.ExecContext(ctx, `
-        CREATE TABLE IF NOT EXISTS withdrawals (
-            id SERIAL PRIMARY KEY,
-            user_id INTEGER NOT NULL REFERENCES users(id),
-            order_number TEXT NOT NULL UNIQUE,
-            sum DECIMAL(10, 2) NOT NULL,
-            processed_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
-        )
-    `)
-
-	if err != nil {
-		return fmt.Errorf("failed to create withdrawals table: %w", err)
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return fmt.Errorf("failed to apply migrations: %w", err)
 	}
-	return tx.Commit()
+
+	return nil
 }
+
+//
+//func (p *PostgresStorage) createTable(ctx context.Context, db *sql.DB) error {
+//	tx, err := db.BeginTx(ctx, nil)
+//	if err != nil {
+//		return err
+//	}
+//	defer tx.Rollback()
+//
+//	_, err = tx.ExecContext(ctx, `
+//		CREATE TABLE IF NOT EXISTS users (
+//		    id SERIAL PRIMARY KEY,
+//			login TEXT NOT NULL UNIQUE,
+//			password_hash TEXT NOT NULL
+//		)
+//	`)
+//	if err != nil {
+//		return fmt.Errorf("failed to create users table: %w", err)
+//	}
+//
+//	_, err = tx.ExecContext(ctx, `
+//		CREATE TABLE IF NOT EXISTS orders (
+//		    id SERIAL PRIMARY KEY,
+//			user_id INTEGER NOT NULL REFERENCES users(id),
+//			number TEXT NOT NULL UNIQUE,
+//			status TEXT NOT NULL,
+//			accrual DECIMAL(10, 2) DEFAULT 0,
+//		    uploaded_at TIMESTAMP WITH TIME ZONE NOT NULL
+//		)
+//	`)
+//	if err != nil {
+//		return fmt.Errorf("failed to create orders table: %w", err)
+//	}
+//
+//	_, err = tx.ExecContext(ctx, `
+//        CREATE TABLE IF NOT EXISTS withdrawals (
+//            id SERIAL PRIMARY KEY,
+//            user_id INTEGER NOT NULL REFERENCES users(id),
+//            order_number TEXT NOT NULL UNIQUE,
+//            sum DECIMAL(10, 2) NOT NULL,
+//            processed_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+//        )
+//    `)
+//
+//	if err != nil {
+//		return fmt.Errorf("failed to create withdrawals table: %w", err)
+//	}
+//	return tx.Commit()
+//}
